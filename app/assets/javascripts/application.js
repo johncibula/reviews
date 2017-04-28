8618092fc8c37ceddcb4db8f1533f8bf1c5fdf77
John Cibula <johnjcibula@gmail.com>
2017-04-27 21:31:26 -0500

    got rid of all react routing

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,8 +12,6 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require react_router
-//= require react_router_ujs
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
