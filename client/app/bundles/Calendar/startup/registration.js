c4cff27161237c825b78800a6ca2d29cd3083acd
Raul <rvasqz86@gmail.com>
2017-04-29 01:54:23 -0500

    fixed calendar

--- client/app/bundles/Calendar/startup/registration.js
+++ client/app/bundles/Calendar/startup/registration.js
@@ -1,5 +1,3 @@
 import ReactOnRails from 'react-on-rails';
 import Calendar from '../components/calendar';
-ReactOnRails.register({Calendar});
-import Calendar from '../components/Calendar';
-ReactOnRails.register({ Calendar });
+ReactOnRails.register({ Calendar});
