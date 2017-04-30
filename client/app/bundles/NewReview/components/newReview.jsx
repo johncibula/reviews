8e0671b533c59a357e9598acfefdf9409a79110c
John Cibula <johnjcibula@gmail.com>
2017-04-30 16:23:17 -0500

    almost working newReview

--- client/app/bundles/NewReview/components/newReview.jsx
+++ client/app/bundles/NewReview/components/newReview.jsx
@@ -11,11 +11,23 @@ export default class NewReview extends React.Component {
                   rating: 0,
                   };
 
-    this.handleChange = this.handleChange.bind(this);
+    this.handleAdminChange = this.handleAdminChange.bind(this);
+    this.handleRecipientChange = this.handleRecipientChange.bind(this);
+    this.handleWillingChange = this.handleWillingChange.bind(this);
+    this.handleRatingChange = this.handleRatingChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleChange(event) {
+  handleRecipientChange(event) {
+      this.setState({recipient_id: event.target.value});
+    }
+  handleAdminChange(event) {
+      this.setState({admin_message: event.target.value});
+    }
+  handleWillingChange(event) {
+      this.setState({willing_to_work: event.target.value});
+    }
+  handleRatingChange(event) {
       this.setState({rating: event.target.value});
     }
 
@@ -31,11 +43,37 @@ export default class NewReview extends React.Component {
     let i = 0
     for (var key in members) {
       choices[i] = <option value={members[key].id}>{members[key].nickname}</option>
-      console.log(members[key].id)
       i += 1
     }
+
     return (
-      <select value={post.recipient_id} onChange={this.handleChange}>
+      <select value={post.recipient_id} onChange={this.handleRecipientChange}>
+        {choices}
+      </select>
+    )
+  }
+
+  listWorkChoices() {
+    const post = this.state
+    const array = ["Never","Possibly","Sure","Yes","Absolutely"]
+    const choices = array.map(function(choice){
+      return <option value={choice}>{choice}</option>
+    })
+    return (
+      <select value={post.willing_to_work} onChange={this.handleWillingChange}>
+        {choices}
+      </select>
+    )
+  }
+
+  listRatingChoices() {
+    const post = this.state
+    const array = [0,1,2,3,4,5]
+    const choices = array.map(function(choice){
+      return <option value={choice}>{choice}</option>
+    })
+    return (
+      <select value={post.rating} onChange={this.handleRatingChange}>
         {choices}
       </select>
     )
@@ -54,6 +92,22 @@ export default class NewReview extends React.Component {
           Who are you reviewing?
           {this.listMembers(this.props.members)}
           </label>
+          <br />
+          <label>
+            What should the cadre know to help them improve?
+            <textarea value={this.state.admin_message} onChange={this.handleAdminChange} />
+          </label>
+          <br />
+          <label>
+            Are you willing to work with this person again?
+            {this.listWorkChoices()}
+          </label>
+          <br />
+          <label>
+            Overall, how would you rate working with them [0 - 5]?
+            {this.listRatingChoices()}
+          </label>
+          <br />
           <input type='submit' value="Submit" />
         </form>
       </div>
