be0d8a929c33064e99295498d32b0b78358cfd5d
Raul <rvasqz86@gmail.com>
2017-05-03 14:21:03 -0500

    partners show up for created reviews

--- client/app/bundles/Post/components/post.jsx
+++ client/app/bundles/Post/components/post.jsx
@@ -18,14 +18,14 @@ export default class Post extends React.Component {
     return (
       <div>
         <td>
-          <PostHeader post={this.props.post} />
+          <PostHeader post={this.props.post} sent={this.props.type==="sent"}/>
         </td>
         <td>
           <PostContent post={this.props.post} />
         </td>
         <td>
           {
-            (this.props.type === "sent") && 
+            this.props.type === "sent" && 
             <button className="button3" onClick={(id) => this.edit(post.id)}>
               Edit
             </button>
@@ -45,7 +45,12 @@ export default class Post extends React.Component {
   var PostHeader = React.createClass({
     render: function() {
       return (
-          <h3>Rating: {this.props.post.rating}</h3>
+        <h3>
+        Rating: {this.props.post.rating}
+        {
+          this.props.sent && <p>Partner: {this.props.post.recipient_id}</p>
+        }
+        </h3>
       );
     }
   });
