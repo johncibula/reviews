96a6c0b7ac49e250d6b16b7848286bb546aa93e0
John Cibula <johnjcibula@gmail.com>
2017-05-03 22:15:21 -0500

    switched order of posts

---
+++ client/app/bundles/Post/startup/registration.js
@@ -0,0 +1,8 @@
+
+import ReactOnRails from 'react-on-rails';
+
+import Post from '../components/Post';
+import PostHeader from '../components/Post';
+import PostContent from '../components/Post';
+
+ReactOnRails.register({ Post,PostHeader,PostContent });
