6bc072f34e0a6d034c94b6b74b8f36a3b3a86dba
Raul <rvasqz86@gmail.com>
2017-05-04 17:57:36 -0500

    seeding file

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,9 +2,87 @@ User.destroy_all
 
 
 User.create!({
-  name: "Jon",
   provider: "github",
-  nickname: "Administrator",
+  nickname: "Instructor",
+  email: "admintest@gmail.com",
+  uid: "8890808908908",
+  repos_url: "https://api.github.com/users/rvasqz86/repos",
+  admin: true
+ 
+})
+User.create!({
+  provider: "github",
+  nickname: "TA",
+  email: "admintest@gmail.com",
+  uid: "8890808908908",
+  repos_url: "https://api.github.com/users/rvasqz86/repos",
+  admin: true
+ 
+})
+
+User.create!({
+  provider: "github",
+  nickname: "Jon",
+  email: "admintest@gmail.com",
+  uid: "8890808908908",
+  repos_url: "https://api.github.com/users/rvasqz86/repos",
+  admin: true
+ 
+})
+
+User.create!({
+  provider: "github",
+  nickname: "James",
+  email: "admintest@gmail.com",
+  uid: "8890808908908",
+  repos_url: "https://api.github.com/users/rvasqz86/repos",
+  admin: true
+ 
+})
+
+User.create!({
+  provider: "github",
+  nickname: "Jones",
+  email: "admintest@gmail.com",
+  uid: "8890808908908",
+  repos_url: "https://api.github.com/users/rvasqz86/repos",
+  admin: true
+ 
+})
+
+User.create!({
+  provider: "github",
+  nickname: "Jamie",
+  email: "admintest@gmail.com",
+  uid: "8890808908908",
+  repos_url: "https://api.github.com/users/rvasqz86/repos",
+  admin: true
+ 
+})
+
+User.create!({
+  provider: "github",
+  nickname: "Johnathon",
+  email: "admintest@gmail.com",
+  uid: "8890808908908",
+  repos_url: "https://api.github.com/users/rvasqz86/repos",
+  admin: true
+ 
+})
+
+User.create!({
+  provider: "github",
+  nickname: "Jackie",
+  email: "admintest@gmail.com",
+  uid: "8890808908908",
+  repos_url: "https://api.github.com/users/rvasqz86/repos",
+  admin: true
+ 
+})
+
+User.create!({
+  provider: "github",
+  nickname: "Jackie",
   email: "admintest@gmail.com",
   uid: "8890808908908",
   repos_url: "https://api.github.com/users/rvasqz86/repos",
