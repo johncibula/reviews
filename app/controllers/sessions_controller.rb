9a039d21d5aab742c22876b09d5feb362458cc0b
Raul <rvasqz86@gmail.com>
2017-05-05 10:04:09 -0500

    Revert "sessions new will not show if logged in"
    This reverts commit 1be67e3d67650814c915fc5f4e8ed9fd431fc78a.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,6 @@
 # sessions_controller.rb
 class SessionsController < ApplicationController
   def create
-    if current_user
-      redirect_to root_url, :notice => "Signed in!"
-    end
     auth = request.env["omniauth.auth"]
     user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
 
