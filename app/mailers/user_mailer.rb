7ca4c2bdc830076567e462f01ff98287b561ba29
Raul <rvasqz86@gmail.com>
2017-05-04 20:48:35 -0500

    debuggin mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,18 +4,18 @@ class UserMailer < ApplicationMailer
   def welcome_email(user)
     @user = user
     @url = 'localhost:3000/sessions/new' 
-    mail(to: @user.email, subject: 'Welcome to Pearing!')   
+    mail(to: "ppearing@gmail.com", subject: 'Welcome to Pearing!')   
   end
 
   def eod_review_email(user)
     @user = user
     @url = 'localhost:3000/sessions/new' 
-    mail(to: @user.email, subject: 'Leave a review for your coding partner!')
+    mail(to: "ppearing@gmail.com", subject: 'Leave a review for your coding partner!')
   end
 
-  # def weekly_email(user)
-  #   @user = user
-  #   @url = 'localhost:3000/sessions/new' 
-  #   mail(to: @user.email, subject: 'Here are your Pairning Reviews')
-  # end
+  def weekly_email(user)
+    @user = user
+    @url = 'localhost:3000/sessions/new' 
+    mail(to: "ppearing@gmail.com", subject: 'Here are your Pairning Reviews')
+  end
 end
