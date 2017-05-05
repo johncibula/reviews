9a46d33e7ccfcf51d4738db8070f7f353ebc4ab7
Raul <rvasqz86@gmail.com>
2017-05-04 20:03:51 -0500

    debuggin mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,9 +87,9 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { :host => 'cp-pairing.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'cp-pairs.herokuapp.com' }
 
-  Rails.application.routes.default_url_options[:host] = 'cp-pairing.herokuapp.com'
+  Rails.application.routes.default_url_options[:host] = 'cp-pairs.herokuapp.com'
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
