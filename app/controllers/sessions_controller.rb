1be67e3d67650814c915fc5f4e8ed9fd431fc78a
Raul <rvasqz86@gmail.com>
2017-05-05 09:52:45 -0500

    sessions new will not show if logged in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,9 @@
 # sessions_controller.rb
 class SessionsController < ApplicationController
   def create
+    if current_user
+      redirect_to root_url, :notice => "Signed in!"
+    end
     auth = request.env["omniauth.auth"]
     user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
 
